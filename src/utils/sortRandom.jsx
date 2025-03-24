d324fd0914ddb163957c415fe55e8b2612a3fede
Prince K <princejaj67@gmail.com>
2025-03-24 18:30:07 +0530

    Added product sorting

--- src/utils/sortRandom.jsx
+++ src/utils/sortRandom.jsx
@@ -20,38 +20,28 @@ const descSort = (arr) => {
 };
 
 const twoWaySort1 = (arr) => {
-  // To store odd numbers
+  
   let odd = [];
-
-  // To store even numbers
   let even = [];
 
   for (let i = 0; i < arr.length; i++) {
-    // If number is even, push it to the even list
-    if (arr[i].id % 2 === 0) {
-      even.push(arr[i]);
-    }
-    // If number is odd, push it to the odd list
-    else {
-      odd.push(arr[i]);
-    }
+    
+    if (arr[i].id % 2 === 0) even.push(arr[i]);
+    else odd.push(arr[i]);
   }
 
-  // Sort the even list in ascending order
   even.sort((a, b) => a - b);
-
-  // Sort the odd list in descending order
+  even.reverse();
+  
   odd.sort((a, b) => b - a);
-
+  odd.reverse();
   let index = 0;
-
-  // First, store the odd numbers in the array
+  
   for (let j = 0; j < odd.length; j++) {
     arr[index] = odd[j];
     index++;
   }
-
-  // Then, store the even numbers in the array
+  
   for (let j = 0; j < even.length; j++) {
     arr[index] = even[j];
     index++;
@@ -60,51 +50,36 @@ const twoWaySort1 = (arr) => {
 };
 
 const twoWaySort2 = (arr) => {
-  // To store odd numbers
+  
   let odd = [];
-
-  // To store even numbers
   let even = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    // If number is even, push it to the even list
-    if (arr[i].id % 2 === 0) {
-      even.push(arr[i]);
-    }
-    // If number is odd, push it to the odd list
-    else {
-      odd.push(arr[i]);
-    }
+  for (let i = 0; i < arr.length; i++) { 
+    if (arr[i].id % 2 === 0) even.push(arr[i]);
+    else odd.push(arr[i]);
+    
   }
-
-  // Sort the even list in ascending order
+  
   even.sort((a, b) => b - a);
-  // Sort the odd list in descending order
+  
   odd.sort((a, b) => a - b);
 
   let index = 0;
-
-  // Then, store the even numbers in the array
   for (let j = 0; j < even.length; j++) {
     arr[index] = even[j];
     index++;
   }
 
-  // First, store the odd numbers in the array
   for (let j = 0; j < odd.length; j++) {
     arr[index] = odd[j];
     index++;
   }
-
+  arr.reverse();
   return arr;
 };
 
 export const sortLocal = (arr)=>{
-	let i = localStorage.getItem("sortOrder");
-	if(!i){
-		i=Math.floor(Math.random()*4);
-		localStorage.setItem("sortOrder",i);
-	}
+  const i = new Date().getHours()%4;
 	if(i==0) return ascSort(arr);
 	else if(i==1) return descSort(arr);
 	else if(i==2) return twoWaySort1(arr);
