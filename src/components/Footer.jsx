6264359b42b41e1a7f46f0b26d47126123221a44
Prince K <princejaj67@gmail.com>
2025-04-06 10:10:18 +0530

    Fixed contacting me

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
             <h3 className='font-semibold tracking-wide sub-heading'>VEGAN'S CORNER</h3>
             <h3 className='font-semibold tracking-wide paratext'>A Non-ISO certified company.</h3>
             <h3 className='font-semibold tracking-wide smalltext mt-2'>© 2024 All Rights Reserved.</h3>
-            <h3 className='font-semibold tracking-wide smalltext mt-2'>For any complain/suggestion <Link to="https://princekb.tech/contact" target='_blank' className='underline hover:text-gray-500'>VISIT ME</Link></h3>
+            <h3 className='font-semibold tracking-wide smalltext mt-2'>For any complain/suggestion <Link to="https://princekb.me/contact" target='_blank' className='underline hover:text-gray-500'>VISIT ME</Link></h3>
         </footer>
 
     </div>
